8c9a8499fd756c11f92802c8dfa83d30f1ef9fc6
chrisrbi <chris.jones@rbi.co.uk>
2024-09-19 15:23:02 +0000

    docker challenge

---
+++ docker-challenge/variables.tf
@@ -0,0 +1,18 @@
+
+variable "internal_port" {
+  description = "internal port to be used"
+  type        = number
+  default     = 9876
+}
+
+variable "external_port" {
+  description = "external port to be used"
+  type        = number
+  default     = 5432
+}
+
+variable "container_name" {
+  description = "name of the container"
+  type        = string
+  default     = "Alta3ResearchWebService"
+}
